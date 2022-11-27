179780383d4f2d24565ec161ab66dc0fa1a424f3
Angelu123 <izerefaifelix@gmail.com>
2022-11-27 17:42:09 +0200

    three js animations

---
+++ src/script.js
@@ -0,0 +1,100 @@
+import "./style.css";
+import * as THREE from "three";
+import gsap from "gsap";
+
+console.log(gsap);
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+/**
+ * Animations
+ */
+
+// let time = Date.now();
+
+// Clock
+
+// const clock = new THREE.Clock();
+
+/**
+ * ANIMATING USING gsap LIBRARY
+ */
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 0 });
+
+const tick = () => {
+  //Time
+
+  //   let currentTime = Date.now();
+
+  //   const deletaTime = currentTime - time;
+
+  //   time = currentTime;
+
+  //   console.log(deletaTime);
+
+  //   const elapsedTime = clock.getElapsedTime();
+
+  //   console.log(elapsedTime);
+  //   mesh.rotation.y += elapsedTime;
+  //   mesh.rotation.y = elapsedTime;
+
+  /**
+   * USING TRIGONOMETRIC FUNCTIONS
+   */
+
+  //   mesh.position.y = Math.sin(elapsedTime);
+  //   mesh.position.x = Math.cos(elapsedTime);
+
+  /**
+   *  ANIMATING A CAMERA
+   */
+
+  //   camera.position.y = Math.sin(elapsedTime);
+  //   camera.position.x = Math.cos(elapsedTime);
+  //   camera.lookAt(mesh.position);
+
+  // making one revolution persecond
+
+  //   mesh.rotation.y = elapsedTime * Math.PI * 2;
+  //Update Objects
+  //   mesh.position.x += 0.01;
+  //   mesh.position.y += 0.01;
+
+  //   mesh.rotation.y += 0.001 * deletaTime;
+  //   mesh.rotation.x += 0.01;
+
+  //Render
+
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
